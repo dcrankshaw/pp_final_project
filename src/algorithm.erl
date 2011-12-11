76bc3fb76e9bcf93fbf55f7fc739bb497c0f0f21
Dan Crankshaw <dscrankshaw@gmail.com>
2011-12-11 02:32:35 -0500

    All algorithm helper methods work. Main method and Map Reduce still untested.

--- src/algorithm.erl
+++ src/algorithm.erl
@@ -27,54 +27,58 @@
 
 -module(algorithm).
 
--export([max_paths/3]).
+-export([max_paths/2, debug/0]).
+%%-export([max_paths/3]).
 
+%%Tested
 process_valstr(ValStr) ->
-    {_, TupList] = regexp:split(ValStr, ":"),
+    TupList = re:split(ValStr, ":"),
     FinalList = lists:map(fun(StrTup) ->
-                            {_, OneTuple} = regexp:split(StrTuple, ","),
+                            OneTuple = re:split(StrTup, ","),
                             [A,B|_] = OneTuple, {A,B} end, TupList),
     FinalList.
 
+%%Tested
 get_biggest_edge(ValidEdges) ->
-    lists:foldl(fun({NewC, NewW}, {OldC, OldW}) ->
+    ReturnList = lists:foldl(fun({NewC, NewW}, {OldC, OldW}) ->
                                 Result = max(list_to_integer(NewW),
                                     list_to_integer(OldW)),
-                                case Result == list_to_integer(NewW) of
+                                case Result =:= list_to_integer(NewW) of
                                     true -> {NewC, NewW};
-                                    false -> {OldC, OldW}
-                                end, {"-1","-1}", ValidEdges).
-    
+                                    _ -> {OldC, OldW}
+                                end end, {"-1","-1"}, ValidEdges),
+    ReturnList.
+
+%%Tested
 find_new_child(EList, ChildList, VertexValStr) ->
 
  %%don't look at edges I have already attempted to claim
-            ValidEdges = lists:filter(fun({PotentialChild, _}) ->
-                                    Result = lists:any(fun({InvalChild,_}) -> PotentialChild =:= InvalChild end, ChildList),
-                                    case Result of
-                                        true -> false;
-                                        _ -> true
-                                    end
-                                end, EList),
-            %%get biggest valid edge                    
-            {NewChild, NewChildWeight = get_biggest_edge(ValidEdges),
-            %%append new child attempt to end of ValString, send message to that child
-            Result = 
-                    case list_to_integer(NewChild) =:= -1 of
-                        false ->
-                            {VertexValStr ++ ":" ++ NewChild ++ "," ++ NewChildWeight,
-                            [{list_to_integer(NewChild), NewChildWeight ++ ":"
-                                ++ "parent"} | []];
-                        _ ->
-                            {VertexValStr, []}
-                    end,
-            Result.
+    ValidEdges = lists:filter(fun({PotentialChild, _}) ->
+                            Result = lists:any(fun({InvalChild,_}) -> PotentialChild =:= InvalChild end, ChildList),
+                            case Result of
+                                true -> false;
+                                _ -> true
+                            end
+                        end, EList),
+    %%get biggest valid edge                    
+    {NewChild, NewChildWeight} = get_biggest_edge(ValidEdges),
+    %%append new child attempt to end of ValString, send message to that child
+    Result = case list_to_integer(NewChild) =:= -1 of
+                false ->
+                    {VertexValStr ++ ":" ++ NewChild ++ "," ++ NewChildWeight,
+                    [{list_to_integer(NewChild), NewChild ++ ":"
+                        ++ NewChildWeight} | []]};
+                _ ->
+                    {VertexValStr, []}
+            end,
+    Result.
             
 
 
 
 max_paths({VertexName, VertexValStr, EList}, InMsgs) ->
     io:format("~n[~p]Recvd msgs : ~p ~n",
-        [VName, InMsgs]),
+        [VertexName, InMsgs]),
 
     GlobalID = list_to_integer(VertexName),
     Snap = GlobalID bsr 32,
@@ -83,28 +87,26 @@ max_paths({VertexName, VertexValStr, EList}, InMsgs) ->
     {VInfo, O, S} = 
     case Snap of
         63 ->
-            {NewVertexVal, OutMsgs} = find_new_child(Elist, ChildList, VertexValStr),
-            NewVInfo = {VertexName, NewVertexVal, Elist},
+            {NewVertexVal, OutMsgs} = find_new_child(EList, ChildList, VertexValStr),
+            NewVInfo = {VertexName, NewVertexVal, EList},
             {NewVInfo, OutMsgs, hold};
         
         _ ->
             %%Do I need to find a new Child?
-            FindNewChildBool = lists:any(fun(CurMessage) ->
-                                    CurMessage =:= "reject" end, InMsgs),
+            FindNewChildBool = lists:member("reject", InMsgs),
             {NewVertexVal, OutMsgsChild} = 
             case FindNewChildBool of
                 true ->
-                     Result = find_new_child(Elist, ChildList, VertexValStr),
-                    {NewVertexVal, OutMsgs};
+                    find_new_child(EList, ChildList, VertexValStr);
                 _ ->
                     {false, []}
             end,
             
             SplitMessages = lists:filter(fun(CurMessage) ->
-                                {_, StrTuple} = regexp:split(CurMessage, ":"),
+                                {_, StrTuple} = re:split(CurMessage, ":"),
                                 length(StrTuple) =:= 2 end, InMsgs),                 
             PossibleParents = [ParentInfo|lists:map(fun(CurMessage) ->
-                                    {_, StrTuple} = regexp:split(CurMessage, ":"),
+                                    {_, StrTuple} = re:split(CurMessage, ":"),
                                     [A,B|_] = StrTuple,
                                     {A,B} end, SplitMessages)],
             NewParent = lists:foldl(fun(CurPos, Max) ->
@@ -112,34 +114,38 @@ max_paths({VertexName, VertexValStr, EList}, InMsgs) ->
                                     {_, MaxWeight} = Max,
                                     case list_to_integer(CurWeight) > list_to_integer(MaxWeight) of
                                         true -> CurPos;
-                                        _ -> MaxPos
-                                    end, ParentInfo, PossibleParents),
+                                        _ -> Max
+                                    end
+                                end, ParentInfo, PossibleParents),
             RejectedParents = lists:filter(fun(CurMessage) ->
-                                        {CurId, CurWeight} = CurPos,
-                                        {MaxId, MaxWeight} = NewParent,
+                                        {CurId, _} = CurMessage,
+                                        {MaxId, _} = NewParent,
                                         CurId =/= MaxId end, PossibleParents),
             RejectMessages = [{ID, "reject"} || {ID, _} <- RejectedParents],
-            OutMsgs = [OutMsgsChild | RejectedParents],
-            FinalVertexValue =
+            OutMsgs = [OutMsgsChild | RejectMessages],
+            FinalVertexVal =
             case NewVertexVal of
                 false -> VertexValStr;
                 _ ->    [_|TempValStr] = process_valstr(NewVertexVal),
                         FinalValStr = [NewParent|TempValStr],
                         FinalTuplesStr = string:join(
-                            lists:map(fun({ID,Weight}) -> ID ++ "," ++ "Weight" end, FinalValStr),
-                            ":"), FinalTupleStr end,
-            NewVInfo = {VertexName, FinalVertexVal, Elis},
+                            lists:map(fun({ID,Weight}) -> ID ++ "," ++ Weight end,
+                            FinalValStr), ":" ),
+                        FinalTuplesStr
+            end,
+            NewVInfo = {VertexName, FinalVertexVal, EList},
             {NewVInfo, OutMsgs, hold} end,
     
-    io:`format("[~p]Sending msgs : ~p ~n", [VName, O]),
+    io:format("[~p]Sending msgs : ~p ~n", [VertexName, O]),
     {VInfo, O, S}.    
                         
             
-
+debug() ->
+    .
 
                         
 
-                        %%Need to fix NewVertexVal
+%%Need to fix NewVertexVal
 
 
 
@@ -150,7 +156,7 @@ max_paths({VertexName, VertexValStr, EList}, InMsgs) ->
 
 
 
-% {_, SplitMessage} = regexp:split(CurMessage, ":"),
+% {_, SplitMessage} = re:split(CurMessage, ":"),
                                         % ThisMessage = lists:any(fun(Message) ->
                                         %         Message =:= "reject" end, SplitMessage),
                                         % ThisMessage end, InMsgs),
